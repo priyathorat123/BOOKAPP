ccead06db767410bdef8449221fc0503a4c2e8e9
SERVER-03 <SERVER-03@DESKTOP-5CQHLH9>
2023-10-13 16:06:01 +0530

    database changes

--- src/main/java/App/Register.java
+++ src/main/java/App/Register.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
-	private static final String query="insert into bookdata(id,bookname,bookedition,bookprice) values(?,?,?) ";
+	 public static final String query="insert into bookdata1(id,bookname,bookedition,bookprice) values(?,?,?,?) ";
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html");
@@ -22,13 +23,35 @@ public class Register extends HttpServlet {
 		String bedi=req.getParameter("bookedi");
 		float bprice=Float.parseFloat(req.getParameter("bookprice"));
 		
+		int count=0;
 		try {
-			Connection con=DriverManager.getConnection("jdbc:mysql:///book","root","password");
+			Class.forName("com.mysql.jdbc.Driver.");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/book","root","password");
 			PreparedStatement pstmt=con.prepareStatement(query);
+			
+			pstmt.setInt(1, 101);
+			pstmt.setString(2,bname);
+			pstmt.setString(3,bedi);
+			pstmt.setFloat(4,bprice);
+			
+			pstmt.execute();
+			count++;
+			con.commit();
+			con.close();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
-			System.out.println("<h1>"+e.getMessage()+"</h1>");
+			System.out.println(e.getMessage());
+			
+		}
+		if(count==1) {
+		    out.println("<h3 style='color:green;text-align:center'>data inserted successfully!!!</h3>");
+		    out.println("<a href='booklist'>Book list</a>");
+		}
+		else {
+			out.println("<h3 style='color:red; text-align:center'>oops something went wrong!!!</h3><br><br>");
+			RequestDispatcher rd=req.getRequestDispatcher("index.html");
+			rd.include(req, res);
 			
 		}
 		
